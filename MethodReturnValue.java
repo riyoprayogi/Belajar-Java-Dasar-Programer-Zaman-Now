c23220b012ea53e9387624262bcbda40aa9688c4
Ryoリズキー <74493992+riyoprayogi@users.noreply.github.com>
2021-04-08 12:05:40 +0800

    Update MethodReturnValue.java

--- MethodReturnValue.java
+++ MethodReturnValue.java
@@ -1,14 +1,35 @@
 public class MethodReturnValue {
     public static void main(String[] args) {
-        var a = 2;
-        var b = 3;
-        var c = sum(a,b);
+        var c = sum(100,100);
         System.out.println(c);
+
+        System.out.println(sum(500, 5000));
+
+        System.out.println(hitung(100, "+", 100));
+        System.out.println(hitung(150, "-", 100));
+        System.out.println(hitung(80, "/", 5));
+        System.out.println(hitung(5, "*", 5));
+        System.out.println(hitung(100, "salah", 100));
 
 
     }
     static int sum(int value1, int value2){
         var total = value1 + value2;
         return total;
+    }
+
+    static int hitung(int value1, String operasi, int value2){
+        switch (operasi){
+            case "+":
+                return value1 + value2;
+            case "-":
+                return value1 - value2;
+            case "/":
+                return value1 / value2;
+            case "*":
+                return value1 * value2;
+            default:
+                return 0;
+        }
     }
 }
